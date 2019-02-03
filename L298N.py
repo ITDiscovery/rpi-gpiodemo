39e4a80ab4ecab2cede4af8c1ee5eb44a887b6c1
Peter Nichols <raspbian@itdiscovery.info>
2019-02-03 11:22:18 -0600

    Update of L298N Code

--- L298N.py
+++ L298N.py
@@ -3,17 +3,35 @@
 
 import RPi.GPIO as GPIO
 import time
+# Setup for signal handling, cleanup on Ctrl-C
+import signal
+GPIO.setwarnings(False)
+
 GPIO.setmode(GPIO.BOARD)
+Dirs=[12,16]
 
-# Set 2 for Data out Motor Drive
-GPIO.setup(16,GPIO.OUT)
-# Set 3 for Data out Motor Drive
-GPIO.setup(18,GPIO.OUT)
+# Set of Data out Motor Drive
+GPIO.setup(Dirs,GPIO.OUT,initial=GPIO.LOW)
 
-while True:
-    GPIO.output(16,True)
-    GPIO.output(18,False)
-    time.sleep(10)
-    GPIO.output(16,False)
-    GPIO.output(18,True)
-    time.sleep(2)
+def sigint_handler(signum, frame):
+	GPIO.output(Dirs[0],GPIO.LOW)
+	GPIO.output(Dirs[1],GPIO.LOW)
+	GPIO.cleanup
+	print("Signal recieved, exiting!")
+	exit()
+
+signal.signal(signal.SIGINT, sigint_handler)
+
+def main():
+	while True:
+		GPIO.output(Dirs[0],GPIO.HIGH)
+		GPIO.output(Dirs[1],GPIO.LOW)
+		print("Forward!")
+		time.sleep(5)
+		GPIO.output(Dirs[0],GPIO.LOW)
+		GPIO.output(Dirs[1],GPIO.HIGH)
+		print("Backward!")
+		time.sleep(5)
+
+if __name__=="__main__":
+	main()
---
+++ LEDDemo.py
@@ -0,0 +1,39 @@
+#Demonstration of 3 Color LED
+#Using GPIO 7 as Red, 11 as Green, 13 as Blue 
+
+import RPi.GPIO as GPIO
+import time 
+GPIO.setmode(GPIO.BOARD)
+
+RedLED=7
+GreenLED=11
+BlueLED=13
+
+# Set 7 for Output as LED for Red
+GPIO.setup(RedLED,GPIO.OUT)
+# Set 11 for Output as LED for Green
+GPIO.setup(GreenLED,GPIO.OUT)
+# Set 13 for Output as LED for Blue
+GPIO.setup(BlueLED,GPIO.OUT)
+
+# Setup can open several ports at once:
+# GPIO.setup([RedLED,GreenLED,BlueLED],GPIO.OUT)
+# or even faster:
+# LEDs=[7,11,13]
+# GPIO.setup(LEDs,GPIO.OUT)
+
+while True:
+	GPIO.output(BlueLED,GPIO.LOW)
+	GPIO.output(RedLED,GPIO.HIGH)
+	#or the fast-way!
+	#GPIO.output(LEDs[1],GPIO.HIGH)
+	print("Red LED!")
+	time.sleep(5)
+	GPIO.output(RedLED,GPIO.LOW)
+	GPIO.output(GreenLED,GPIO.HIGH)
+	print("Green LED!")
+	time.sleep(5)
+	GPIO.output(GreenLED,GPIO.LOW)
+	GPIO.output(BlueLED,GPIO.HIGH)
+	print("Blue LED!")
+	time.sleep(5)
